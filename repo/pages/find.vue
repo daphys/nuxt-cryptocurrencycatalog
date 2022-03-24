c5ea5a3f612894164085f418ba9bd7ee190ccd54
Vlad <floppahear@gmail.com>
2022-03-24 22:24:52 +0300

    first pages + find page start

---
+++ repo/pages/find.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <input v-model="dataFromInput" type="text">
+        <button @click="getData">+</button>
+        <div v-for="r in response" :key="r">
+            {{r.name}}
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            dataFromInput: [],
+        }
+    },
+    methods: {
+        async getData(dataFromInput) {
+            const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${this.dataFromInput}`)
+            return response
+        },
+    },
+    }
+</script>
+
+<style>
+
+</style>
---
+++ repo/pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Welcome to Cryptonomicon</h1>
+  
+
+    <div v-for="i in ip" :key="i">
+      <div class="elem">
+      <h1>{{i.name}}</h1>
+      </div> 
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Get from '/components/Get.vue'
+import axios from 'axios'
+export default {
+  async asyncData({ $axios }) {
+    const ip = await $axios.$get('https://api.coingecko.com/api/v3/coins/list')
+    return { ip }
+  },
+  components: {
+    Get,
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<style scoped>
+  .elem {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid black;
+  
+  }
+</style>
